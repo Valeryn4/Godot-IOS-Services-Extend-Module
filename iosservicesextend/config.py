57d04e20d71a46c25295efc3fff1902186fa1269
Питанов Валера <dtho47@yandex.ru>
2020-03-03 20:15:28 +1000

    module build

--- iosservicesextend/config.py
+++ iosservicesextend/config.py
@@ -1,6 +1,7 @@
-def can_build(plat):
-	return plat=="iphone"
+def can_build(env, platform):
+   return platform =="iphone"
 
 def configure(env):
-	if env['platform'] == "iphone":
-		env.Append(LINKFLAGS=['-ObjC',])
+   pass
+   #if env['platform'] == "iphone":
+      #env.Append(LINKFLAGS=['-ObjC',])
